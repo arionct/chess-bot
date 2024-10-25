dcb6f8b12aa4cfe80b2339f72a4c1856316beb70
inhye <inhye@bu.edu>
2024-10-25 15:12:02 -0400

    update order

--- CustomMoveOrderer.java
+++ CustomMoveOrderer.java
@@ -1,13 +1,17 @@
 package src.pas.chess.moveorder;
 
 
+// SYSTEM IMPORTS
 import edu.bu.chess.search.DFSTreeNode;
+
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
-import src.pas.chess.heuristics.CustomHeuristics;
 
-// SYSTEM IMPORTS
+import src.pas.chess.heuristics.CustomHeuristics;
+// JAVA PROJECT IMPORTS
+import src.pas.chess.moveorder.DefaultMoveOrderer;
 
 public class CustomMoveOrderer
     extends Object
@@ -22,8 +26,49 @@ public class CustomMoveOrderer
 	 */
 	public static List<DFSTreeNode> order(List<DFSTreeNode> nodes)
 	{
-		Collections.sort(nodes, Comparator.comparingDouble(CustomHeuristics::getMaxPlayerHeuristicValue).reversed());
-        return nodes;
+		List<DFSTreeNode> captureNodes = new LinkedList<>();
+		List<DFSTreeNode> promotionNodes = new LinkedList<>();
+		List<DFSTreeNode> centerControlNodes = new LinkedList<>();
+		List<DFSTreeNode> otherNodes = new LinkedList<>();
+
+		for (DFSTreeNode node : nodes) {
+			if (node.getMove() != null) {
+				switch (node.getMove().getType()) {
+					case CAPTUREMOVE:
+						captureNodes.add(node);
+						break;
+					case PROMOTEMOVE:
+						promotionNodes.add(node);
+						break;
+					default:
+						if (isCenterControlMove(node)) {
+							centerControlNodes.add(node);
+						} else {
+							otherNodes.add(node);
+						}
+						break;
+				}
+			} else {
+				otherNodes.add(node);
+			}
+		}
+
+		captureNodes.addAll(promotionNodes);
+		captureNodes.addAll(centerControlNodes);
+		captureNodes.addAll(otherNodes);
+		return captureNodes;
+	}
+
+	/**
+	 * Determines if a move controls the center of the board.
+	 * @param node The DFSTreeNode containing the move.
+	 * @return True if the move controls the center, otherwise false.
+	 */
+	private static boolean isCenterControlMove(DFSTreeNode node) {
+		Coordinate targetPosition = node.getMove().getTargetPosition();
+		int x = targetPosition.getXPosition();
+		int y = targetPosition.getYPosition();
+		return (x >= 4 && x <= 5 && y >= 4 && y <= 5);
 	}
 
 }
